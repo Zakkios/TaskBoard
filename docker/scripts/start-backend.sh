97726a96113489353c9a43792f22ffde04eb54b3
Zakkios <laskari.zakaria@gmail.com>
2025-04-22 10:50:43 +0200

    fix: ajout des migrations dans le sh backend et adaptation des fichier d'environnement

--- docker/scripts/start-backend.sh
+++ docker/scripts/start-backend.sh
@@ -9,5 +9,13 @@ fi
 echo "🚀 Installing PHP dependencies..."
 composer install
 
+# Auto-migrations activées uniquement si ENV différent de "prod"
+if [ "$ENVIRONMENT" != "prod" ]; then
+  echo "🧪 ENVIRONMENT=$ENVIRONMENT → Running database migrations..."
+  php bin/console doctrine:migrations:migrate --no-interaction
+else
+  echo "⚠️ ENVIRONMENT=prod → Skipping database migrations"
+fi
+
 echo "🚀 Starting Symfony server..."
 symfony server:start --no-tls --allow-http --port=8000 --listen-ip=0.0.0.0
