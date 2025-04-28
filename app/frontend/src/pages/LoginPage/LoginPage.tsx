9ae5bd4eb9ebc30ec07488d4852d67f968af6a4a
Zakkios <laskari.zakaria@gmail.com>
2025-04-28 19:35:44 +0200

    feat: prémice de la page login, creation du css et de LoginPage.tsx

---
+++ app/frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,19 @@
+import Logo from "./../../assets/logo.png";
+
+export default function LoginPage() {
+  return (
+    <div className="relative min-h-screen flex flex-col-reverse items-center justify-center p-4">
+      <img
+        src={Logo}
+        alt="logo"
+        className="absolute top-10 w-[200px] max-w-[40%]"
+      />
+      <div className="w-full max-w-[90%] md:max-w-[500px] bg-[#E2DEFF] p-8 rounded-2xl shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-4">Login Page</h1>
+        <p className="text-center mb-6">
+          Please enter your credentials to log in.
+        </p>
+      </div>
+    </div>
+  );
+}
