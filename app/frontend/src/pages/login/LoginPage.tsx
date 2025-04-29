4275ede12c8e1bd44cbd9943ea2628c0e7661f32
Zakkios <laskari.zakaria@gmail.com>
2025-04-29 12:00:09 +0200

    feat: implémentation des nouveaux composant input et button

--- app/frontend/src/pages/login/LoginPage.tsx
+++ app/frontend/src/pages/login/LoginPage.tsx
@@ -1,4 +1,6 @@
 import Logo from "@/shared/assets/logo.png";
+import Input from "@/shared/ui/Input/Input";
+import Button from "@/shared/ui/Button/Button";
 
 export default function LoginPage() {
   return (
@@ -8,11 +10,30 @@ export default function LoginPage() {
         alt="logo"
         className="absolute top-10 w-[200px] max-w-[40%]"
       />
-      <div className="w-full max-w-[90%] md:max-w-[500px] bg-[#E2DEFF] p-8 rounded-2xl shadow-lg">
-        <h1 className="text-2xl font-bold text-center mb-4">Login Page</h1>
-        <p className="text-center mb-6">
-          Please enter your credentials to log in.
-        </p>
+      <div className="flex items-center justify-center w-[800px] max-w-[90%] h-[400px] bg-primary p-8 rounded-2xl shadow-lg">
+        <form className="flex flex-col items-center gap-4 w-full max-w-[450px]">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            classNameParent="w-full"
+            required
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Mot de passe"
+            classNameParent="w-full"
+            required
+          />
+          <Button
+            type="submit"
+            variant="secondary"
+            className="w-[300px] max-w-full"
+          >
+            Connexion
+          </Button>
+        </form>
       </div>
     </div>
   );
