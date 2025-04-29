8119a12239d209482e5f0c85bec5c62237beb5cf
Zakkios <laskari.zakaria@gmail.com>
2025-04-29 10:13:38 +0200

    chore: ajout des alias frontend

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   server: {
     watch: {
       usePolling: true,
@@ -13,4 +17,4 @@ export default defineConfig({
     strictPort: true,
     port: 3000,
   },
-})
+});
