13914cd4c70cd2219572239630cb9d6e0fa2511f
Zakkios <laskari.zakaria@gmail.com>
2025-04-29 11:59:49 +0200

    feat: creation du composant button et du type button

---
+++ app/frontend/src/shared/ui/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import clsx from "clsx";
+import { ButtonProps } from "./Button.types";
+
+const Button: FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  className,
+  ...props
+}) => {
+  return (
+    <button
+      className={clsx(
+        "px-6 py-3 rounded-lg font-semibold transition-colors duration-200",
+        variant === "secondary" &&
+          "bg-secondary text-white cursor-pointer hover:bg-blue-700",
+        variant === "tertiary" &&
+          "bg-tertiary text-black cursor-pointer hover:bg-gray-700",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
