10f127c81d85a04200678cc3c46f94c69ac0f955
Zakkios <laskari.zakaria@gmail.com>
2025-04-29 09:48:13 +0200

    chore: creation de toute les pages et d'un index.ts qui exporte chacune d'elle

---
+++ app/frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as LoginPage } from "./login/LoginPage";
+export { default as RegisterPage } from "./register/RegisterPage";
+export { default as DashboardPage } from "./dashboard/DashboardPage";
+export { default as StatisticsPage } from "./statistics/StatisticsPage";
+export { default as NotFoundPage } from "./not-found/NotFoundPage";
