dd211bfa2b7a8cd8eda5d611bf7e62dd2fb6376e
Zakkios <laskari.zakaria@gmail.com>
2025-05-14 18:44:27 +0200

    feat: ajout du contrôleur de déconnexion et mise à jour de la gestion des cookies JWT

--- app/backend/src/Controller/RegisterController.php
+++ app/backend/src/Controller/RegisterController.php
@@ -29,7 +29,6 @@ class RegisterController extends AbstractController
         /** @var RegisterUserInput $data */
         $data = $serializer->deserialize($request->getContent(), RegisterUserInput::class, 'json');
 
-        // Validation des données
         $errors = $validator->validate($data);
         if (count($errors) > 0) {
             $errorMessages = [];
@@ -43,17 +42,14 @@ class RegisterController extends AbstractController
             return new JsonResponse(['errors' => $errorMessages], 400);
         }
 
-        // Vérification unicité de l'email
         if ($em->getRepository(User::class)->findOneBy(['email' => $data->email])) {
             return new JsonResponse(['errors' => ['email' => ['Cet email est déjà utilisé.']]], 400);
         }
 
-        // Vérification unicité du username
         if ($em->getRepository(User::class)->findOneBy(['username' => $data->username])) {
             return new JsonResponse(['errors' => ['username' => ['Ce nom d\'utilisateur est déjà utilisé.']]], 400);
         }
 
-        // Création de l'utilisateur
         $user = new User();
         $user->setEmail($data->email);
         $user->setUsername($data->username);
--- app/backend/src/EventListener/AuthenticationSuccessListener.php
+++ app/backend/src/EventListener/AuthenticationSuccessListener.php
@@ -7,23 +7,36 @@ use Symfony\Component\HttpFoundation\Cookie;
 
 class AuthenticationSuccessListener
 {
+    private string $cookieDomain;
+    private bool $cookieSecure;
+
+    public function __construct(
+        string $cookieDomain,
+        bool   $cookieSecure,
+    ) {
+        $this->cookieDomain = $cookieDomain;
+        $this->cookieSecure = $cookieSecure;
+    }
     /**
      * @param AuthenticationSuccessEvent $event
      */
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
     {
         $data = $event->getData();
+        if (!isset($data['token'])) {
+            return;
+        }
+
         $response = $event->getResponse();
-        $domain = $_ENV['JWT_DOMAIN'];
-        $secure = $_ENV['JWT_SECURE'];
+        $response->headers->clearCookie('jwt');
 
         $cookie = new Cookie(
             'jwt',
             $data['token'],
             (new \DateTime('+1 hour'))->format('D, d M Y H:i:s T'),
             '/',
-            $domain,
-            $secure,
+            $this->cookieDomain,
+            $this->cookieSecure,
             true,
             false,
             'strict'
