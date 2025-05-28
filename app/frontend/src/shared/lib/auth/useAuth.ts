7fd5a2ce156587cae9b7b34f914ab547f37790f9
Zakkios <laskari.zakaria@gmail.com>
2025-05-28 10:03:45 +0200

    feat: ajout de l'interface User et mise à jour de useAuth pour inclure l'utilisateur

--- app/frontend/src/shared/lib/auth/useAuth.ts
+++ app/frontend/src/shared/lib/auth/useAuth.ts
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
 import api from "@/shared/api/api";
+import User from "@/shared/lib/auth/User.type";
 
-export function useAuth(): { loading: boolean; isAuthenticated: boolean } {
-  const [state, setState] = useState({ loading: true, isAuthenticated: false });
+export default function useAuth(): {
+  loading: boolean;
+  isAuthenticated: boolean;
+  user: User | null;
+} {
+  const [state, setState] = useState({
+    loading: true,
+    isAuthenticated: false,
+    user: null as User | null,
+  });
 
   useEffect(() => {
     api
       .get("me")
       .then((res) =>
-        setState({ loading: false, isAuthenticated: res.status === 200 })
+        setState({
+          loading: false,
+          isAuthenticated: res.status === 200,
+          user: res.data as User,
+        })
       )
-      .catch(() => setState({ loading: false, isAuthenticated: false }));
+      .catch(() =>
+        setState({ loading: false, isAuthenticated: false, user: null })
+      );
   }, []);
 
   return state;
