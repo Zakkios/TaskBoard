15aacba1856e70003ef388d461d3f4d214b85077
Zakkios <laskari.zakaria@gmail.com>
2025-05-28 10:01:17 +0200

    feat: ajout du composant Topbar avec affichage du nom d'utilisateur

---
+++ app/frontend/src/features/navigation/topbar/Topbar.tsx
@@ -0,0 +1,21 @@
+import Button from "@/shared/ui/Button/Button";
+import { FaUser } from "react-icons/fa";
+import User from "@/shared/lib/auth/User.type";
+
+type TopbarProps = { user: User | null };
+
+export default function Topbar({ user }: TopbarProps) {
+  return (
+    <div className="fixed flex justify-end items-center bg-black h-24 w-full">
+      <div>
+        <Button
+          variant="dark-gray"
+          className="flex items-center gap-2 py-2 px-5 mr-8 rounded-4xl"
+        >
+          {user?.username || "Guest"}
+          <FaUser />
+        </Button>
+      </div>
+    </div>
+  );
+}
