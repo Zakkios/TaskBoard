331dd3b266de9af8932c8ed524eb2ec10a42cbcc
Zakkios <laskari.zakaria@gmail.com>
2025-05-28 10:03:08 +0200

    feat: ajout de la page de statistiques avec chargement et affichage de l'utilisateur

--- app/frontend/src/pages/statistics/StatisticsPage.tsx
+++ app/frontend/src/pages/statistics/StatisticsPage.tsx
@@ -1,3 +1,19 @@
+import Sidebar from "@/features/navigation/sidebar/Sidebar";
+import Topbar from "@/features/navigation/topbar/Topbar";
+import useAuth from "@/shared/lib/auth/useAuth";
+import Loader from "@/shared/ui/Loader/Loader";
+
 export default function StatisticsPage() {
-  return <div></div>;
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      <Topbar user={user} />
+      <Sidebar />
+    </div>
+  );
 }
