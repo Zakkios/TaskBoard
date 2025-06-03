2a1110593c1d26108edd5298362f79d8c79f54fb
Zakkios <laskari.zakaria@gmail.com>
2025-06-03 17:55:53 +0200

    fix: correction du chemin d'importation pour les colonnes dans DashboardPage

--- app/frontend/src/pages/dashboard/DashboardPage.tsx
+++ app/frontend/src/pages/dashboard/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "@/features/navigation/sidebar/Sidebar";
 import Topbar from "@/features/navigation/topbar/Topbar";
-import COLUMNS from "@/features/taskBoard/config/columns";
+import COLUMNS from "@/features/taskBoard/consts/columns";
 import TaskBoard from "@/features/taskBoard/ui/TaskBoard";
 import useAuth from "@/shared/lib/auth/useAuth";
 import Loader from "@/shared/ui/Loader/Loader";
