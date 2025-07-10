6b005757dfd8260f32beeea4f7b4ab129bfe9e5c
Zakkios <laskari.zakaria@gmail.com>
2025-07-10 08:48:58 +0200

    chore: mise à jour des dépendances frontend et amélioration des commandes ESLint dans package.json
    - Suppression de l’ancienne version d’ESLint et mise à jour vers la version 8.57.1
    - Mise à jour de @eslint/js vers la version 9.30.1
    - Mise à jour de eslint-plugin-react-hooks vers la version 4.6.0
    - Mise à jour de globals vers la version 16.3.0
    - Mise à jour de typescript-eslint vers la version 8.36.0
    - Ajout des commandes eslint et eslint-fix dans le package.json racine pour faciliter le lint dans le conteneur frontend

--- app/frontend/eslint.config.js
+++ app/frontend/eslint.config.js
@@ -1,28 +1,37 @@
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+
+      // Tes règles personnalisées (optionnelles)
+      'linebreak-style': ['error', 'unix'],
+      'no-plusplus': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+]
