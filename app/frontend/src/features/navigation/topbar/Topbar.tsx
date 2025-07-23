7b06ddff79f92ba16890543304fb605fb1a981c2
Zakkios <laskari.zakaria@gmail.com>
2025-07-23 15:59:32 +0200

    feat: Mise à jour de la Topbar avec ajout du logo et changement de style de bouton

--- app/frontend/src/features/navigation/topbar/Topbar.tsx
+++ app/frontend/src/features/navigation/topbar/Topbar.tsx
@@ -1,13 +1,18 @@
-import { User, Button } from "@/shared";
+import { User, Button, Logo } from "@/shared";
+import { NavLink } from "react-router";
 import { FaUser } from "react-icons/fa";
 
 type TopbarProps = { user: User | null };
 
 export function Topbar({ user }: TopbarProps) {
   return (
-    <div className="fixed flex justify-end items-center bg-black h-24 w-full z-20">
+    <div className="fixed flex justify-between items-center bg-white h-24 w-full z-20 px-6 border-b border-[#DEDEDE]">
+      <NavLink to="/" className={"flex pl-8 items-center gap-3"}>
+        <Logo size={20} />
+        <h1 className="text-2xl font-extrabold">TaskBoard</h1>
+      </NavLink>
       <div>
-        <Button variant="dark-gray">
+        <Button variant="white">
           {user?.username || "Guest"}
           <FaUser />
         </Button>
