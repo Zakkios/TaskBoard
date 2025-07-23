63898f1d3fbc0b9a9d05a467ba84f73b06b981ab
Zakkios <laskari.zakaria@gmail.com>
2025-07-23 14:54:06 +0200

    feat: Refonte du frontend pour la page login

--- app/frontend/src/pages/login/LoginPage.tsx
+++ app/frontend/src/pages/login/LoginPage.tsx
@@ -1,16 +1,11 @@
-import Logo from "@/shared/assets/logo.png";
 import { LoginForm } from "@/features/auth";
-import { TextLink } from "@/shared";
+import { Logo, TextLink } from "@/shared";
 
 export default function LoginPage() {
   return (
-    <div className="relative min-h-screen flex flex-col-reverse items-center justify-center p-4">
-      <img
-        src={Logo}
-        alt="logo"
-        className="absolute top-10 w-[100px] max-w-[40%]"
-      />
-      <div className="flex items-center justify-center w-[800px] max-w-[90%] h-[400px] bg-primary p-8 rounded-2xl shadow-lg">
+    <div className="relative min-h-screen flex flex-col-reverse items-center justify-center p-4 bg-gradient-to-br from-gradient-blue to-gradient-purple">
+      <Logo size={46} />
+      <div className="flex items-center justify-center w-[800px] max-w-[90%] h-[400px] bg-white/30 border-solid border-1 border-white p-8 rounded-2xl shadow-lg">
         <div className="flex flex-col items-center justify-center w-full h-full gap-4">
           <LoginForm />
           <div className="text-center">
