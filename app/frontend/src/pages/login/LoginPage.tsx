d7416aa057abe0423610cf5444aaae57cc7286b6
Zakkios <laskari.zakaria@gmail.com>
2025-07-23 16:01:00 +0200

    feat: Mise a jour des pages login et register en suivant la nouvelle structure du composant Logo

--- app/frontend/src/pages/login/LoginPage.tsx
+++ app/frontend/src/pages/login/LoginPage.tsx
@@ -4,7 +4,12 @@ import { Logo, TextLink } from "@/shared";
 export default function LoginPage() {
   return (
     <div className="relative min-h-screen flex flex-col-reverse items-center justify-center p-4 bg-gradient-to-br from-gradient-blue to-gradient-purple">
-      <Logo size={46} />
+      <div className="absolute top-10 flex flex-col items-center justify-center gap-4 text-center pb-3">
+        <div>
+          <Logo size={46} />
+        </div>
+        <h1 className="text-3xl font-bold text-white">TaskBoard</h1>
+      </div>
       <div className="flex items-center justify-center w-[800px] max-w-[90%] h-[400px] bg-white/30 border-solid border-1 border-white p-8 rounded-2xl shadow-lg">
         <div className="flex flex-col items-center justify-center w-full h-full gap-4">
           <LoginForm />
